3db351016efd4fe8fb51a63f24b7f56b69ddd81d
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2020-11-25 11:13:34 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -30,25 +30,25 @@ define([
         connection.trigger('requestEndpoints');
     }
 
-  function initialize(data) {
-        console.log("Initializing data data: "+ JSON.stringify(data));
+    function initialize(data) {
+        console.log("Initializing data data: " + JSON.stringify(data));
         if (data) {
             payload = data;
-        }    
+        }
 
         var hasInArguments = Boolean(
             payload['arguments'] &&
             payload['arguments'].execute &&
             payload['arguments'].execute.inArguments &&
             payload['arguments'].execute.inArguments.length > 0
-         );
+        );
 
         var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
 
-        console.log('Has In arguments: '+JSON.stringify(inArguments));
+        console.log('Has In arguments: ' + JSON.stringify(inArguments));
 
-        $.each(inArguments, function (index, inArgument) {
-            $.each(inArgument, function (key, val) {
+        $.each(inArguments, function(index, inArgument) {
+            $.each(inArgument, function(key, val) {
 
                 if (key === 'accountSid') {
                     $('#accountSID').val(val);
@@ -64,7 +64,7 @@ define([
 
                 if (key === 'body') {
                     $('#messageBody').val(val);
-                }                                                               
+                }
 
             })
         });
@@ -77,15 +77,15 @@ define([
 
     }
 
-    function onGetTokens (tokens) {
+    function onGetTokens(tokens) {
         // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
-        console.log("Tokens function: "+JSON.stringify(tokens));
+        console.log("Tokens function: " + JSON.stringify(tokens));
         //authTokens = tokens;
     }
 
-    function onGetEndpoints (endpoints) {
+    function onGetEndpoints(endpoints) {
         // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
-        console.log("Get End Points function: "+JSON.stringify(endpoints));
+        console.log("Get End Points function: " + JSON.stringify(endpoints));
     }
 
     function save() {
@@ -105,9 +105,9 @@ define([
 
         payload['metaData'].isConfigured = true;
 
-        console.log("Payload on SAVE function: "+JSON.stringify(payload));
+        console.log("Payload on SAVE function: " + JSON.stringify(payload));
         connection.trigger('updateActivity', payload);
 
-    }                    
+    }
 
 });
